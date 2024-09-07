108dd0adc1fec5a275b38cf70c9bb67b30ab0561
Pranav Saini <71985059+pranavsaini-maker@users.noreply.github.com>
2024-09-08 01:47:56 +0530

    Create 2727. Is Object Empty.js

---
+++ solutions/2727. Is Object Empty.js
@@ -0,0 +1,46 @@
+/*
+PROBLEM:
+Given an object or an array, return if it is empty.
+
+An empty object contains no key-value pairs.
+An empty array contains no elements.
+You may assume the object or array is the output of JSON.parse.
+
+ 
+
+Example 1:
+
+Input: obj = {"x": 5, "y": 42}
+Output: false
+Explanation: The object has 2 key-value pairs so it is not empty.
+Example 2:
+
+Input: obj = {}
+Output: true
+Explanation: The object doesn't have any key-value pairs so it is empty.
+Example 3:
+
+Input: obj = [null, false, 0]
+Output: false
+Explanation: The array has 3 elements so it is not empty.
+ 
+
+Constraints:
+
+obj is a valid JSON object or array
+2 <= JSON.stringify(obj).length <= 10**5
+*/
+
+//SOLUTION:
+
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if(Array.isArray(obj)) {
+        return obj.length === 0;
+    }else {
+        return Object.keys(obj).length === 0;
+    }
+};
